944264225626bbf6c6885c3432ca70939d83a5ff
yangwb <314430343@qq.com>
2021-10-19 11:58:05 +0800

    阿里excel操作工具类

---
+++ demo-server/src/main/java/com/example/demoserver/test/controller/ExcelUtilsController.java
@@ -0,0 +1,30 @@
+package com.example.demoserver.test.controller;
+
+import com.alibaba.excel.metadata.Sheet;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ExcelUtilsController {
+
+//    public static void main(String[] args) {
+//        String filePath = "/home/chenmingjian/Downloads/学生表.xlsx";
+//        //第一个1代表sheet1, 第二个1代表从第几行开始读取数据，行号最小值为0
+//        Sheet sheet = new Sheet(1, 1);
+//        List<Object> objects = ExcelUtil.readLessThan1000Row(filePath,sheet);
+//
+//    }
+//
+//    public void getExeet1(){
+//        String filePath = "/home/chenmingjian/Downloads/学生表.xlsx";
+//        Sheet sheet = new Sheet(2, 0);
+//        List<Object> objects = ExcelUtil.readLessThan1000Row(filePath,sheet);
+//    }
+//
+//    public void getExeet2(){
+//        String filePath = "/home/chenmingjian/Downloads/学生表.xlsx";
+//        Sheet sheet = new Sheet(2, 0);
+//        List<Object> objects = ExcelUtil.readLessThan1000Row(filePath,sheet);
+//    }
+}
